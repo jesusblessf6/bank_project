991c3a9966e26a2213d078bea0b7ac856055e9ba
jesusblessf6 <jesusblessf6@gmail.com>
2014-08-28 17:53:11 +0800

    update

--- back/crawlers/cmb.js
+++ back/crawlers/cmb.js
@@ -6,6 +6,7 @@ exports.start = function(){
 	var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 
 	driver.get('http://best.cmbchina.com/Shop/Search.aspx?citycode=0021');
+
 };
 
 
