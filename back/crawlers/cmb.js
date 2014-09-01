55973ce950c05c74ee312553514acfa6aa401d29
jesusblessf6 <jesusblessf6@gmail.com>
2014-09-01 17:54:13 +0800

    update

--- back/crawlers/cmb.js
+++ back/crawlers/cmb.js
@@ -4,9 +4,79 @@ exports.start = function(){
 	var async = require('async');
 
 	var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+	var pageNum = 0;
 
-	driver.get('http://best.cmbchina.com/Shop/Search.aspx?citycode=0021');
+	async.series([
 
+			//open the coupon main page
+			function(callback){
+				driver.get('http://best.cmbchina.com/Shop/Search.aspx?citycode=0021').then(callback);
+			},
+
+			//get the total page number
+			function(callback){
+				getPageNum(driver, function(pn){
+					pageNum = pn;
+					console.log(pageNum);
+					callback();
+				});
+			},
+
+			//go through all the pages
+			function(callback){
+				async.timesSeries(pageNum, function(n, next){
+					traversal(n, driver, next);
+				}, function(err){
+					if(err){
+						console.dir(err);
+					}
+					callback();
+				});
+			},
+
+			//end
+			function(callback){
+				driver.close();
+				driver.quit();
+				callback();
+			}
+		], 
+
+		function(err){
+			if(err){
+				console.dir(err);
+			}
+		}
+	);
 };
 
+function getPageNum(driver, callback){
+	//get the total page number
+	var pagenum = 0;
+	driver.findElement({className: 'pageinfo'}).then(function(element){
+		element.getText().then(function(text){
+			var ps = text.split('/');
+			pagenum = Number(ps[1].replace('页', ''));
+		}).then(function(){
+			callback(pagenum);
+		});
+	});
+}
 
+//traversal pages, get all available activity ids
+function traversal(index, driver, callback){
+	//console.log(index);
+	driver.get('http://best.cmbchina.com/Shop/Search.aspx?citycode=0021&pageno=' + (index+1));
+	driver.findElements({className: 'shopname'}).then(function(elements){
+		elements.forEach(function(e){
+			
+		})
+	});
+
+	callback();
+}
+
+//get activity detail
+function detail(activityId, driver){
+
+}
--- back/models/activity.js
+++ back/models/activity.js
@@ -5,9 +5,11 @@ var activitySchema = new db.Schema({
 	end: Date,
 	description: String,
 	shopName: String,
-	address: String
+	address: String,
+	activityId: String
 });
 
 var Activity = db.mongoose.model('Activity', activitySchema);
 
-module.exports = Activity;
+exports.model = Activity;
+exports.schema = activitySchema;
