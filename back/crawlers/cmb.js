fcc0389c828e08dac25cfaf759b38f262ef3cbc0
jesusblessf6 <jesusblessf6@gmail.com>
2014-09-03 16:14:26 +0800

    update

--- back/crawlers/cmb.js
+++ back/crawlers/cmb.js
@@ -72,6 +72,7 @@ function traversal(index, driver, callback){
 	async.series([
 
 			function(callback){
+				console.log("page:" + (index+1));
 				driver.get('http://best.cmbchina.com/Shop/Search.aspx?citycode=0021&pageno=' + (index+1)).then(callback);
 			},
 
@@ -136,38 +137,31 @@ function detail(activityId, driver, next){
 			},
 
 			function(callback){
-				driver.findElement({className: 'list'}).then(function(listDiv){
-					listDiv.findElements({tagName: 'tr'}).then(function(trs){
-						trs.forEach(function(tr, index, arr){
-							tr.findElements({tagName: 'td'}).then(function(tds){
-								tds[0].getText().then(function(tt){
-									if(tt.trim() === "服务电话："){
-										tds[1].getText().then(function(tt2){
-											activity.tel = tt2.trim();
-										});
-									}else if(tt.trim() === "商户地址："){
-										tds[1].getText().then(function(tt2){
-											activity.address = tt2.trim();
-										});
-									}else if(tt.trim() === "优惠日期："){
-										tds[1].getText().then(function(tt2){
-											var arr1 = tt2.split('(开始)');
-											var start = new Date(arr1[0].trim());
-											var end = new Date(arr1[1].replace('(截止)',''));
-											activity.start = start;
-											activity.end = end;
-										});
-									}
-								});
-							});
-						});
-					});
+
+				driver.findElements({className : 'content_detail'}).then(function(contents){
+					getShopInfo(contents[0], driver, activity);
+					getActivityInfo(contents[1],driver,activity);
 				}).then(callback);
+
 			},
 
 			function(callback){
 				console.dir(activity);
-				callback();
+				var activityModel = require('../models/activity').model;
+
+				activityModel.count({activityId : activity.activityId}, function(err, count){
+					if(count == 0){
+						//persistence
+						activity.save(function(err){
+							if(err){
+								console.dir(err);
+							}
+						});
+					}
+					callback();
+				});
+
+				
 			}
 
 		], function(err){
@@ -175,5 +169,57 @@ function detail(activityId, driver, next){
 			console.dir(err);
 		}
 		next();
+	});
+}
+
+function getShopInfo(contentDiv, driver, activity){
+	contentDiv.findElement({className: 'list'}).then(function(listDiv){
+		listDiv.findElements({tagName: 'tr'}).then(function(trs){
+			trs.forEach(function(tr, index, arr){
+				tr.findElements({tagName: 'td'}).then(function(tds){
+					tds[0].getText().then(function(tt){
+						if(tt.trim() === "服务电话："){
+							tds[1].getText().then(function(tt2){
+								activity.tel = tt2.trim();
+							});
+						}else if(tt.trim() === "商户地址："){
+							tds[1].getText().then(function(tt2){
+								activity.address = (tt2.split('\n'))[0].trim();
+							});
+						}else if(tt.trim() === "优惠日期："){
+							tds[1].getText().then(function(tt2){
+								var arr1 = tt2.split('(开始)');
+								var start = new Date(arr1[0].trim());
+								var end = new Date(arr1[1].replace('(截止)',''));
+								activity.start = start;
+								activity.end = end;
+							});
+						}
+					});
+				});
+			});
+		});
+	});
+}
+
+function getActivityInfo(contentDiv, driver, activity){
+	contentDiv.findElement({className: 'list'}).then(function(listDiv){
+		listDiv.findElements({tagName: 'tr'}).then(function(trs){
+			trs.forEach(function(tr, index, arr){
+				tr.findElements({tagName: 'td'}).then(function(tds){
+					tds[0].getText().then(function(tt){
+						if(tt.trim() === "持卡优惠："){
+							tds[1].getText().then(function(tt2){
+								activity.description = tt2.trim();
+							});
+						}else if(tt.trim() === "优惠细则："){
+							tds[1].getText().then(function(tt2){
+								activity.rule = tt2.trim();
+							});
+						}
+					});
+				});
+			});
+		});
 	});
 }
