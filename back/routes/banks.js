2a66e4602b25ccbf9c936872d51629519512716c
jesusblessf6 <jesusblessf6@gmail.com>
2014-09-10 17:25:08 +0800

    update

--- back/routes/banks.js
+++ back/routes/banks.js
@@ -6,9 +6,15 @@ router.get('/', function(req, res){
 });
 
 router.get('/:bankname', function(req, res){
+	//console.log("bankname: " + req.params.bankname);
 	//res.send(req.params.bankname);
 	res.render('banks/' + req.params.bankname, {title: ''});
 	
+});
+
+router.get('/:bankname/:actionname', function(req, res){
+	//console.log("bankname: " + req.params.bankname + "; actionname: " + req.params.actionname);
+	res.render('banks/'+ req.params.bankname + req.params.actionname, {title: ''});
 });
 
 module.exports = router;
