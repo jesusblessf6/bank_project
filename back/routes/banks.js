59390ad4c823b358e45daa5622e9dea936d91545
jesusblessf6 <jesusblessf6@gmail.com>
2014-09-11 16:52:09 +0800

    update

--- back/routes/banks.js
+++ back/routes/banks.js
@@ -1,12 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+/*
+routes:
+	=== banks ===
+	GET '/' : banks-index;
+	POST '/' : banks-create;
+	GET '/new' : banks-new;
+	GET '/:bankcode' : display bank info by code
+	GET '/:bankcode/edit' : edit bank info by code
+	POST '/:bankcode' : update bank info by code
+	DELETE '/:bankcode' : delete bank by code
+	=== activities of current bank ===
+	GET '/:bankcode/activities' : bank's activities - index
+	POST '/:bankcode/activities' : bank's activities - create
+	GET '/:bankcode/activities/new' : open the activity-new interface
+	GET '/:bankcode/activities/:activityid' : display activity info
+	GET '/:bankcode/activities/:activityid/edit' : edit activity
+	POST '/:bankcode/activities/:activityid' : update activity
+	DELETE '/:bankcode/activities/:activityid' : delete activity
+*/
+
+//GET '/' : banks-index;
 router.get('/', function(req, res){
 	res.render('banks/index', {title: '银行'});
 });
 
+//POST '/' : banks-create;
+router.post('/', function(req, res){
+
+});
+
+router.get('/new', function(req, res){
+	//console.log('get new page');
+});
+
 router.get('/:bankname', function(req, res){
 	var Bank = require('../models/bank').model;
+	//console.log('get bank');
 	res.render('banks/' + req.params.bankname, {title: ''});
 	
 });
